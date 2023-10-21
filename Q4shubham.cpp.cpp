e381c8dafb4c3e6b7bd300e634945cd71a8b0e02
Shubhpanwar23 <shubhpanwar232@gmail.com>
2023-10-21 15:33:53 +0530

    addedcpp

---
+++ Q4shubham.cpp.cpp
@@ -0,0 +1,16 @@
+#include<bits/std++.ch>
+
+class student() {
+		int studid;
+		int marks1,makrs2,marks3;
+		
+	student(){
+		
+	}
+	void getid(int id){
+		this.studid=id;
+	}
+	void get_marks(int m1, int m2, int m3){
+		
+	}
+}
